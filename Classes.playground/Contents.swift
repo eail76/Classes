535c2629d027768182124fc9478becc34d990a69
eail76 <eail@modelo.edu.mx>
2015-06-12 09:09:46 -0500

    Tarea 3 Classes

---
+++ Classes.playground/Contents.swift
@@ -0,0 +1,16 @@
+class Animal{
+    
+    var name = "default"
+    var age = 0;
+    
+    func getDetails() -> String{
+        return "this animals name is \(name) it is \(age) years old."
+    }
+    
+    
+}
+
+var myAnimal = Animal()
+myAnimal.name = "Woofy"
+myAnimal.age = 2
+myAnimal.getDetails()
